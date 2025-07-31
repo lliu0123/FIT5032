d7f5b66bb21328bb8a604f434e821dfbda9f2336
Lingwei Liu <lliu0123@student.monash.edu>
2025-07-31 18:16:54 +1000

    add MyComponent.vue component

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="my-component">
+    <h2>New Module 🚀</h2>
+    <p>原神，启动！！！！！！</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyComponent',
+}
+</script>
+
+<style scoped>
+.my-component {
+  background-color: #f0f8ff;
+  padding: 16px;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+}
+</style>
